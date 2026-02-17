e51be9a9cba4a2ff1ba023b45fc066e9956e4275
Damandundel <damian.georgiev08@gmail.com>
2026-02-17 09:35:15 +0200

    All the code

---
+++ ServerProject.Demo/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using WebServer.Server;
+using WebServer.Server.Responses;
+
+namespace WebServer.demo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var server = new HttpServer(routes =>
+
+                routes
+                .MapGet("/", new TextResponse("Hello from the server!"))             
+                .MapGet("/HTML", new HtmlResponse("<h1>HTML response</h1>"))         
+                .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"));  
+            });
+            server.Start();
+        }
+    }
+}
