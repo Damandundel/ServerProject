eae0e7c9d3078152d6a94adf0b898a7f72b566ed
Damandundel <damian.georgiev08@gmail.com>
2026-02-17 10:04:14 +0200

    g

--- ServerProject.Demo/StartUp.cs
+++ ServerProject.Demo/StartUp.cs
@@ -8,13 +8,13 @@ namespace WebServer.demo
         public static void Main()
         {
             var server = new HttpServer(routes =>
-
+            {
                 routes
-                .MapGet("/", new TextResponse("Hello from the server!"))             
-                .MapGet("/HTML", new HtmlResponse("<h1>HTML response</h1>"))         
-                .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"));  
+                    .MapGet("/", new TextResponse("Hello from the server!"))
+                    .MapGet("/HTML", new HtmlResponse("<h1>HTML response</h1>"))
+                    .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"));
             });
-            server.Start();
+            
         }
     }
 }
